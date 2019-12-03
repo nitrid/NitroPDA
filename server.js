6b4338e119a5a3f378e92ba7101b929128fa6877
nitrid <alikemalkaraca@gmail.com>
2019-12-03 09:45:43 +0300

    .

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var express = require('express');
 var app = express();
 
 app.use('/',express.static(__dirname + "/www"));
-app.listen(8080);
+app.listen(8081);
 
 //******** LOG **********************************************************************/
 var trueLog = console.log;
