3a53395689d3cbfb252df1e470ac3f20b1bd2039
nitrid <alikemalkaraca@gmail.com>
2020-06-04 16:48:19 +0300

    .

--- www/lib/Param.js
+++ www/lib/Param.js
@@ -24,7 +24,7 @@ var Param = [
 			"SiparisOnayListele": "-1",
 			"EslestirmeEksiyeDusme": "0",
 			"BTYaziciTip": "CORDOVABT",
-			"BTYaziciAdi": "nitrogen",
+			"BTYaziciAdi": "BT-SPP",
 			"FirmaListe": ""
 		},
 		"Sayim": {
