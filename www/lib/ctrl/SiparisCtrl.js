50ad43ddd1565c8ea29885c631b77d04d0908d18
MAHİR GÜNDÜZ <mhrgndz>
2020-06-16 16:08:50 +0300

    ..

--- www/lib/ctrl/SiparisCtrl.js
+++ www/lib/ctrl/SiparisCtrl.js
@@ -2217,9 +2217,7 @@ function SiparisCtrl($scope,$window,$timeout,db,$filter)
         }
         else
         {
-            var  InsertData =  [$scope.Aciklama1,$scope.Aciklama2,$scope.Aciklama3,$scope.Aciklama4,$scope.Aciklama5,$scope.AciklamaGuid]
-            console.log($scope.AciklamaGuid)
-            
+            var  InsertData =  [$scope.Aciklama1,$scope.Aciklama2,$scope.Aciklama3,$scope.Aciklama4,$scope.Aciklama5,$scope.AciklamaGuid]            
             db.ExecuteTag($scope.Firma,'AciklamaUpdate',InsertData,function(InsertResult)
             {
                 if(typeof(InsertResult.result.err) == 'undefined')
@@ -2228,11 +2226,7 @@ function SiparisCtrl($scope,$window,$timeout,db,$filter)
                     console.log(InsertResult)
                 }
             })
-            
-        
         }
-        
-
     }
     $scope.AciklamaGetir = function()
     {
@@ -2249,7 +2243,6 @@ function SiparisCtrl($scope,$window,$timeout,db,$filter)
                 $scope.Aciklama4 = $scope.AciklamaSatir[0].egk_evracik4,
                 $scope.Aciklama5 = $scope.AciklamaSatir[0].egk_evracik5,
                 $scope.AciklamaGuid = $scope.AciklamaSatir[0].egk_Guid
-                console.log($scope.AciklamaGuid)
             }
 
         })
